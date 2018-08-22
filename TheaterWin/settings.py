c308b05f21c42b5623443bbfb4ce6dcfccd328ec
theaterwinner <theaterwinner@gmail.com>
2018-08-23 00:00:30 +0900

    pythonanywhere 에 등록하기 위한 setting

--- TheaterWin/settings.py
+++ TheaterWin/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'v4t3@&*^zrj(+fxjpx_=2i_+$royxy5u98)2!8^pa@2l9%dxvj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 # Application definition
 
